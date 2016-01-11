c70f195b7a25f9e8de96f8fc1f435d61972ccf96
Marta Wyszynska <marwyszynska@gmail.com>
2016-01-12 00:59:42 +0100

    [#3, #14] add flash messages, url for event sign up & sign out, refactor events index view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,12 +4,13 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def destroy
-    @booking = Booking.where(:user_id => current_user.id, :event_id => @event.id).first
-    @booking.destroy
+    @booking = Booking.find_by(id: params[:id])
     if @booking.destroy
       redirect_to events_path
+      flash[:success] = 'wypisano z wyjazdu'
     else #gdyby byla formatka to:  render action: 'destroy'
-      redirect_to events_path, error: @errors.message
+      redirect_to events_path
+      flash[:error] = 'wystąpił błąd'
     end
   end
 
@@ -17,8 +18,10 @@ class BookingsController < ApplicationController
     @booking = current_user.bookings.new(event_id: @event.id)
     if @booking.save
       redirect_to events_path
+      flash[:success] = 'zapisano na wyjazd'
     else #gdyby byla formatka to:  render action: 'new'
       redirect_to events_path, error: @errors.message
+      flash[:error] = 'wystąpił błąd'
     end
   end
 
