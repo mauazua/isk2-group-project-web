0ff188c3b9bb4e893e10270e8c324e2e8efbbe5b
mwrotek <monika.wrotek@wp.pl>
2016-02-13 02:37:10 +0100

    change price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219233314) do
+ActiveRecord::Schema.define(version: 20160213012658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20151219233314) do
     t.datetime "end_date"
     t.string   "location"
     t.string   "country"
-    t.integer  "price"
+    t.decimal  "price"
     t.integer  "capacity"
   end
 
